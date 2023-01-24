4db0a9b0cd16a276ddbba33ff74583da7f91addd
Frank Casanova <frankcasanova.info@gmail.com>
2023-01-24 14:25:57 +0100

    first endpoint created and first template served

---
+++ backend/core/config.py
@@ -0,0 +1,8 @@
+#config.py
+
+class Settings:
+    PROJECT_NAME: str = "Job Board"
+    PROJECT_VERSION: str = "1.0.0"
+
+
+settings = Settings()
---
+++ backend/main.py
@@ -0,0 +1,24 @@
+# main.py
+
+from fastapi import FastAPI
+from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
+
+
+def include_router(app):
+    app.include_router(general_pages_router)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME,
+                  version=settings.PROJECT_VERSION)
+    include_router(app)
+    return app
+
+
+app = start_application()
+
+
+# @app.get("/") #remove this, It is no longer needed.
+# def hello_api():
+#     return {"msg":"Hello API"}
