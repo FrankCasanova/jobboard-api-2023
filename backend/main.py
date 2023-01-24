7851f17143271179b3177f3f126275da92369540
Frank Casanova <frankcasanova.info@gmail.com>
2023-01-24 14:39:52 +0100

    logo added

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
-# main.py
-
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles  # new
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
 
@@ -9,16 +8,16 @@ def include_router(app):
     app.include_router(general_pages_router)
 
 
+def configure_static(app):  # new
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME,
                   version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)  # new
     return app
 
 
 app = start_application()
-
-
-# @app.get("/") #remove this, It is no longer needed.
-# def hello_api():
-#     return {"msg":"Hello API"}
