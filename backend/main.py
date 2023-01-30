8298ee365d26aff5151b30b2b238c99f348ead65
Frank Casanova <frankcasanova.info@gmail.com>
2023-01-30 16:39:07 +0100

    conexion to a database done.

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,29 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles  # new
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine  # new
+from db.base_class import Base  # new
 
 
 def include_router(app):
-    app.include_router(general_pages_router)
+	app.include_router(general_pages_router)
 
 
-def configure_static(app):  # new
+def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+def create_tables():  # new
+	Base.metadata.create_all(bind=engine)
+
+
 def start_application():
-    app = FastAPI(title=settings.PROJECT_NAME,
-                  version=settings.PROJECT_VERSION)
-    include_router(app)
-    configure_static(app)  # new
-    return app
+	app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+	include_router(app)
+	configure_static(app)
+	create_tables()  # new
+	return app
 
 
 app = start_application()
