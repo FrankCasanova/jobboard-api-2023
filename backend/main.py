3e536d656683ad73ad926783f4e58d4246a27f9a
frank-casanova <frankcasanova.info@gmail.com>
2023-01-30 23:48:09 +0100

    some adjustemets before commit

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
-from db.session import engine  # new
-from db.base import Base  # new
-
-
-def create_tables():
-	print("create_tables")
-	Base.metadata.create_all(bind=engine)
+from db.session import engine   #new
+from db.base_class import Base  #new
 
 
 def include_router(app):
@@ -19,16 +15,18 @@ def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-def create_tables():  # new
+def create_tables():           #new
 	Base.metadata.create_all(bind=engine)
 
-
+	
 def start_application():
-	app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 	include_router(app)
 	configure_static(app)
-	create_tables()  # new
+	create_tables()       #new
 	return app
 
-
 app = start_application()
+
+
+
