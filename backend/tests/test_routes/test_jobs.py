b334e74727c2b22c0749b09e0ad557ed5cd7ac70
Frank Casanova <frankcasanova.info@gmail.com>
2023-02-06 21:05:30 +0100

    post job and test post job created

---
+++ backend/tests/test_routes/test_jobs.py
@@ -0,0 +1,16 @@
+
+
+
+def test_create_job(client):
+    data = {
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA,NY",
+        "description": "python",
+        "date_posted": "2022-03-20"
+    }
+    response = client.post("/jobs/create-job/", json=data)
+    assert response.status_code == 200
+    assert response.json()["company"] == "doogle"
+    assert response.json()["description"] == "python"
--- backend/tests/test_routes/test_users.py
+++ backend/tests/test_routes/test_users.py
@@ -1,4 +1,4 @@
-import json
+
 
 
 def test_create_user(client):
