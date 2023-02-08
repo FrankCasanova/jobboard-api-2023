9bca4c26cfe35f40d0b13c746909a528c876b9a2
Frank Casanova <frankcasanova.info@gmail.com>
2023-02-08 14:29:15 +0100

    route for read a job created, database logic created, implementation test created

--- backend/main.py
+++ backend/main.py
@@ -7,24 +7,24 @@ from db.session import engine   #new
 from db.base_class import Base  #new
 
 
-def include_router(app):
+def include_router(app) -> None:
 	app.include_router(api_router)
 
 
-def configure_static(app):
+def configure_static(app) -> None:
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-def create_tables():
+def create_tables() -> None:
     print('Creating tables.')
     Base.metadata.create_all(bind=engine)
 
 	
-def start_application():
+def start_application() -> FastAPI():
 	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 	include_router(app)
 	configure_static(app)
-	create_tables()       #new
+	create_tables()       
 	return app
 
 app = start_application()
--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -14,3 +14,19 @@ def test_create_job(client):
     assert response.status_code == 200
     assert response.json()["company"] == "doogle"
     assert response.json()["description"] == "python"
+
+
+def test_read_job(client):  
+    data = {
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA,NY",
+        "description": "python",
+        "date_posted": "2022-03-20"
+    }
+    response = client.post("/jobs/create-job/", json=data)
+
+    response = client.get("/jobs/get/1/")
+    assert response.status_code == 200
+    assert response.json()['title'] == "SDE super"
