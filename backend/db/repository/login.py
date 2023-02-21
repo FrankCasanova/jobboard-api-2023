be718d6504a164e3c92ddae0c38732ef98d489ef
Frank Casanova <frankcasanova.info@gmail.com>
2023-02-21 16:02:42 +0100

    authentication logic has been created

---
+++ backend/db/repository/login.py
@@ -0,0 +1,8 @@
+# db >repository > login.py
+from db.models.users import User
+from sqlalchemy.orm import Session
+
+
+def get_user(username: str, db: Session):
+    user = db.query(User).filter(User.email == username).first()
+    return user
