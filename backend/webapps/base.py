3855b66be1f3919d17aad8f5e31bd455bdbcd22d
Frank Casanova <frankcasanova.info@gmail.com>
2023-02-23 20:15:21 +0100

    user login and job post added

--- backend/webapps/base.py
+++ backend/webapps/base.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .auth import route_login
 from .jobs import route_jobs
 from .users import route_users
 
@@ -7,3 +8,4 @@ from .users import route_users
 api_router = APIRouter()
 api_router.include_router(route_jobs.router, prefix="", tags=["job-webapp"])
 api_router.include_router(route_users.router, prefix="", tags=["users-webapp"])
+api_router.include_router(route_login.router, prefix="", tags=["auth-webapp"])
