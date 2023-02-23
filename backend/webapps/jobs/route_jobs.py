b84c666e33141e936a787399b883cf6c144f5e05
Frank Casanova <frankcasanova.info@gmail.com>
2023-02-23 15:21:30 +0100

    jobs details view included

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -1,4 +1,5 @@
 from db.repository.jobs import list_jobs
+from db.repository.jobs import retreive_job
 from db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
@@ -17,4 +18,13 @@ async def home(request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse(
         "general_pages/homepage.html",
         {"request": request, "jobs": jobs},
+    )
+
+
+@router.get("/details/{id}")
+def job_detail(id: int, request: Request, db: Session = Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+    return templates.TemplateResponse(
+        "jobs/detail.html",
+        {"request": request, "job": job},
     )
