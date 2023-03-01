0fbc8e3fdafb6c6642a667fa228e447a7cd01204
Frank Casanova <frankcasanova.info@gmail.com>
2023-03-02 00:44:17 +0100

    show the username in the template added

--- backend/apis/version1/route_users.py
+++ backend/apis/version1/route_users.py
@@ -1,10 +1,16 @@
+from db.models.users import User
 from db.repository.users import create_new_user
 from db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import Request
+from fastapi.security.utils import get_authorization_scheme_param
 from schemas.users import ShowUser
 from schemas.users import UserCreate
 from sqlalchemy.orm import Session
+
+from .route_login import get_current_user_from_token
+
 
 router = APIRouter()
 
@@ -13,3 +19,16 @@ router = APIRouter()
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user=user, db=db)
     return user
+
+
+@router.get("/me", response_model=ShowUser)
+def read_users_me(
+    request: Request,
+    db: Session = Depends(get_db),
+):
+    token = request.cookies.get("access_token")
+    scheme, param = get_authorization_scheme_param(
+        token,
+    )  # scheme will hold "Bearer" and param will hold actual token value
+    current_user: User = get_current_user_from_token(token=param, db=db)
+    return current_user
