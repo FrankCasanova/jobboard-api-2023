15c7b931ddb5f05b4b6616b2f1112e5187075de3
Frank Casanova <67984454+FrankCasanova@users.noreply.github.com>
2023-10-13 21:36:24 +0200

    Update base.py
    update to deploy

--- backend/db/base.py
+++ backend/db/base.py
@@ -1,4 +1,4 @@
 # db > base.py
-from db.base_class import Base
-from db.models.jobs import Job
-from db.models.users import User
+from .db.base_class import Base
+from .db.models.jobs import Job
+from .db.models.users import User
