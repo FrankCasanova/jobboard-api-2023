2a8dabe5dfde4f39383d83d161d460839c4b4112
Frank Casanova <67984454+FrankCasanova@users.noreply.github.com>
2023-10-13 20:59:49 +0200

    Update config.py
    settings for Render.com

--- backend/core/config.py
+++ backend/core/config.py
@@ -11,16 +11,14 @@ class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
 
-    PGPORT: str = os.getenv("PGPORT")
-    PGUSER: str = os.getenv("PGUSER")
-    PGPASSWORD = os.getenv("PGPASSWORD")
+    # PGPORT: str = os.getenv("PGPORT")
+    # PGUSER: str = os.getenv("PGUSER")
+    # PGPASSWORD = os.getenv("PGPASSWORD")
     # POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
     # default postgres port is 5432
-    PGHOST: str = os.getenv("PGHOST", 5432)
-    PGDATABASE: str = os.getenv("PGDATABASE", "tdd")
-    DATABASE_URL = (
-        f"postgresql://{ PGUSER }:{ PGPASSWORD }@{ PGHOST }:{ PGPORT }/{ PGDATABASE }"
-    )
+    # PGHOST: str = os.getenv("PGHOST", 5432)
+    # PGDATABASE: str = os.getenv("PGDATABASE", "tdd")
+    DATABASE_URL: str = os.getenv("PGDATABASE")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
