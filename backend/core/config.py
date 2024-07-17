b41d72d8de363aa520c80cdb5034c4e168e34d92
Frank Casanova <frankcasanova.info@gmail.com>
2024-07-17 21:39:50 +0200

    persistent_database added

--- backend/core/config.py
+++ backend/core/config.py
@@ -11,17 +11,18 @@ class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
 
-    # PGPORT: str = os.getenv("PGPORT")
-    # PGUSER: str = os.getenv("PGUSER")
-    # PGPASSWORD = os.getenv("PGPASSWORD")
-    # POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    PGPORT: str = os.getenv("POSTGRES_PORT")
+    PGUSER: str = os.getenv("POSTGRES_USER")
+    PGPASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
     # default postgres port is 5432
-    # PGHOST: str = os.getenv("PGHOST", 5432)
-    # PGDATABASE: str = os.getenv("PGDATABASE", "tdd")
-    # DATABASE_URL: str = os.getenv("DATABASE_URL")
-    SECRET_KEY: str = 'ASDFASDFAEROAIPSUDFHAWEFLDVAJDFH'
+    PGHOST: str = os.getenv("POSTGRES_HOST", 5432)
+    PGDATABASE: str = os.getenv("POSTGRES_DATABASE", "tdd")
+    DATABASE_URL: str = os.getenv("POSTGRES_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    print(SECRET_KEY)
     ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 1
+    ACCESS_TOKEN_EXPIRE_MINUTES = 10
     TEST_USER_EMAIL = "test@example.com"
 
 
