a355a9b8467b070ff36946154a694fb6c3641eb8
Frank Casanova <frankcasanova.info@gmail.com>
2024-07-22 16:09:32 +0200

    update entrypoint

--- backend/main.py
+++ backend/main.py
@@ -45,4 +45,4 @@ def start_application() -> FastAPI:
 app = start_application()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
